65eaea840a879803e90a9ab0b1fa89b0cd8166cc
Hayden Manning <haydenmanning@aol.com>
2018-02-01 17:13:35 -0600

    JSON sorted / sort.js fixed

--- server/sort.js
+++ server/sort.js
@@ -1,57 +1,38 @@
-const marketData = require("./../jitaTypes.json"); // top 1000 types in jita (no names)
-const itemNames = require("./../itemNames.json"); // all types in game
-const matches = [];
-const namesID = [];
-const newItemNames = [];
+// THIS HAS NOTHING TO DO WITH HOW THE SERVER OPERATES THIS FILE IS JUST MEANT TO PRODUCE jitaItemInfo.json WHICH IT HAS I AM KEEPING THIS HERE FOR REFERENCE
 
-// sort low to high
-function sortNumber(a, b) {
-  return a - b;
+const jitaData = require("./../jitaIdData.json"); // top 5000 types in jita (no names) pages 1-5
+const itemNames = require("./../itemNames.json"); // all types in game
+const matches = []; // will hold the matches found in findMatches
+const pureJitaData = []; // will hold a duplicate removed array of jitaData
+
+// checks for duplicates in a json data file and removes duplicates
+function removeDupes(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (pureJitaData.indexOf(arr[i]) == -1) {
+      pureJitaData.push(arr[i]);
+    }
+  }
+  console.log(pureJitaData);
+  console.log(pureJitaData.length);
+  return pureJitaData;
 }
 
-function findMatches(arr, arrObj) {}
+removeDupes(jitaData);
 
-// function findMatches(arr, names) {
-//   for (let i = 0; i < arr.length; i++) {
-//     marketID.push(arr[i].type_id);
-//   }
-//   for (let i = 0; i < names.length; i++) {
-//     namesID.push(names[i].TYPEID);
-//   }
-//   marketID.sort(sortNumber);
-//   namesID.sort(sortNumber);
-//   for (let i = namesID.length - 1; i >= 0; i--) {
-//     let count = 0;
-//     for (let x = marketID.length - 1; x >= 0; x--) {
-//       if (namesID[i] === marketID[x]) {
-//         console.log(namesID[i]);
-//         marketID.splice(x, 1);
-//         break;
-//       } else {
-//         count++;
-//       }
-//       if (count === marketID.length) {
-//         console.log("splicing");
-//         namesID.splice(i, 1);
-//       }
-//     }
-//   }
-//   console.log(`MARKET LENGTH: ${marketID.length}`);
-//   console.log(`NAMES LENGTH: ${namesID.length} DATA: ${namesID}`);
-//   return namesID;
-// }
+// finds matching type ids and pushes matches to a new array
+//   arr = jitaData [id, id, id, id, id]
+//   arrObj = itemNames -- [{id, itemname}, {id, itemname}, {id, itemname}]
+function findMatches(arr, arrObj) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let x = 0; x < arrObj.length; x++) {
+      if (arr[i] === arrObj[x].TYPEID) {
+        console.log(`Match found ${arrObj[x].TYPEID}`);
+        matches.push(arrObj[x]);
+      }
+    }
+  }
+  console.log(matches);
+  return matches;
+}
 
-// findMatches(marketData, itemNames);
-
-// function toNewArr(arr, json, newArr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     for (let x = 0; x < json.length; x++) {
-//       if (arr[i] === json[x].TYPEID) {
-//         newArr.push(json[i]);
-//       }
-//     }
-//   }
-//   console.log(newArr[newArr.length - 1]);
-// }
-
-// toNewArr(namesID, itemNames, newItemNames);
+findMatches(pureJitaData, itemNames);
