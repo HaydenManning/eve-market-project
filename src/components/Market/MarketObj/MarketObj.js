43b703154285e63ad2fb45a8774bea960aab7f08
Hayden Manning <haydenmanning@aol.com>
2018-02-01 19:46:31 -0600

    implemented history, calculating daily moving average

--- src/components/Market/MarketObj/MarketObj.js
+++ src/components/Market/MarketObj/MarketObj.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./MarketObj.css";
+import axios from "axios";
 
 class MarketObj extends Component {
   constructor(props) {
@@ -8,31 +9,70 @@ class MarketObj extends Component {
       favorite: false, // determines if item has been favorite or not
       itemName: "", // told by Market.js
       typeID: "", // told by Market.js
-      lowSell: "", // pulls from orders api
-      highBuy: "", // pulls from orders api
-      dailyAvg: "", // pulls from history api
-      volume: "", // pulls from history api
-      stateUp: 0 // counts the amount of state updates
+      sell: [], // dump of sell orders api
+      buy: [], // dump of buy orders api
+      lowSell: "", // found by finding lowest sell from sell
+      highBuy: "", // found by finding highest buy from buy
+      history: [], // dump of history api
+      dailyAvg: "", // CALCULATION Daily ISK in Jita for item
+      volume: "", // Daily Volume in Jita for item
+      avgPrice: "", // Daily average price used with volume to find daily moving average gotten from history
+      stateUp: 0, // counts the amount of state updates
+      histApi: 0 // limits the history call to 1 pull (the initial pull)
     };
   }
 
-  // updateState() {
-  //   this.setState({
-  //     itemName: this.props.typeName,
-  //     typeID: this.props.id
-  //   });
-  //   let x = this.state.stateUp;
-  //   x++;
-  // }
+  //updates state for items that are not being directly imported into this component
+  updateState() {
+    if (this.state.stateUp === 0) {
+      this.setState({
+        itemName: this.props.typeName,
+        typeID: this.props.id,
+        stateUp: 1
+      });
+    }
+  }
 
   // favorite mechanic, attach to button and it favorites the item -- needs an unfavorite also
-  favorite() {
+  // favorite() {
+  //   this.setState({
+  //     favorite: true
+  //   });
+  // }
+
+  // call for market history of specific type id
+  getHistory() {
+    if (this.state.histApi === 0) {
+      axios
+        .get(
+          `http://127.0.0.1:3005/api/eve/markets/history/${this.state.typeID}`
+        )
+        .then(results => {
+          this.setState({
+            history: results.data,
+            histApi: 1,
+            volume: results.data[results.data.length - 1].volume,
+            avgPrice: results.data[results.data.length - 1].average
+          });
+          this.calcDailyAvg();
+        })
+        .catch(console.log);
+    }
+  }
+
+  calcDailyAvg() {
+    let tempVar = 0;
+    tempVar =
+      parseInt(this.state.volume, 10) * parseInt(this.state.avgPrice, 10);
+    tempVar = tempVar.toLocaleString();
     this.setState({
-      favorite: true
+      dailyAvg: tempVar
     });
   }
 
   render() {
+    this.updateState();
+    this.getHistory();
     return (
       <div className="market-obj">
         <ul>
@@ -47,11 +87,11 @@ class MarketObj extends Component {
             <h2>TYPEID</h2>
             <p>{this.props.id}</p>
           </li>
-          {/*<li>
-            <h2>AVERAGE PRICE</h2>
-           <p>{this.props.avg >= 0 ? this.props.avg : "0"}</p>
-          </li>
           <li>
+            <h2>DAILY MOVING AVERAGE</h2>
+            <p>{`${this.state.dailyAvg} ISK`}</p>
+          </li>
+          {/*<li>
             <h2>ADJUSTED PRICE</h2>
            <p>{this.props.adj}</p>
          </li> */}
