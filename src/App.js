465050b64518cc3e6a7e35742b8552709153d559
Hayden Manning <haydenmanning@aol.com>
2018-02-02 18:50:53 -0600

    removed logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Market from "./components/Market/Market";
 import Header from "./components/Header/Header";
