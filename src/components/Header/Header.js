b06fcec279823d2e5774d62f71264ef16b884f02
Hayden Manning <haydenmanning@aol.com>
2018-02-04 03:07:36 -0600

    header finished/NEED FOOTER/NEED CSS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,17 @@ class Header extends Component {
   render() {
     return (
       <div className="header">
-        <h1 className="title">Welcome to Eve Ships - Market Edition!</h1>
+        <div className="header-left">
+          <h1 className="title">Welcome to Eve Ships - Market Edition!</h1>
+        </div>
+        <div className="header-right">
+          <a href="http://github.com/HaydenManning" target="_blank">
+            <i class="fab fa-3x fa-github" id="github" />
+          </a>
+          <a href="mailto:thaydenmanning@gmail.com" target="_blank">
+            <i class="fas fa-3x fa-envelope" id="email" />
+          </a>
+        </div>
       </div>
     );
   }
