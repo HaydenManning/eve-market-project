8345c1e43b2731c95b00703250e690f021314c9d
Hayden Manning <haydenmanning@aol.com>
2018-02-06 11:33:12 -0600

    stateless functional

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,32 +2,30 @@ import React, { Component } from "react";
 import("./Header.css");
 
 // this is my stateless component
-class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        <div className="header-left">
-          <h1 className="title">Welcome to the Eve Market!</h1>
-        </div>
-        <div className="header-right">
-          <a
-            href="http://github.com/HaydenManning"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i class="fab fa-3x fa-github" id="github" />
-          </a>
-          <a
-            href="mailto:thaydenmanning@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i class="fas fa-3x fa-envelope" id="email" />
-          </a>
-        </div>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-left">
+        <h1 className="title">Welcome to the Eve Market!</h1>
       </div>
-    );
-  }
-}
+      <div className="header-right">
+        <a
+          href="http://github.com/HaydenManning"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fab fa-3x fa-github" id="github" />
+        </a>
+        <a
+          href="mailto:thaydenmanning@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fas fa-3x fa-envelope" id="email" />
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
